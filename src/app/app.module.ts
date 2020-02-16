21d351c307fc8e4e8a38cbabb455533b569799f0
giresse <ajanohpelkins18@gmail.com>
2020-02-16 08:24:29 +0200

    Add base homepage component and bootstrap styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './component/homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
