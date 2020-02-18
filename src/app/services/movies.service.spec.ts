3651115191b536475a42a5ce1fcd80426be62822
giresse <ajanohpelkins18@gmail.com>
2020-02-18 19:41:45 +0200

    Add unit test for service methods.

--- src/app/services/movies.service.spec.ts
+++ src/app/services/movies.service.spec.ts
@@ -4,10 +4,14 @@ import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule ]
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        MoviesService,       
+    ]
     });
     service = TestBed.inject(MoviesService);
   });
@@ -15,14 +19,29 @@ describe('MoviesService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should escape unsafe character', () => {  
+    const result1 = service.handleSpecialCharsHtml("&") ;
+    const result2 = service.handleSpecialCharsHtml("<") ;
+    const result3 = service.handleSpecialCharsHtml(">") ;
+    const result4 = service.handleSpecialCharsHtml('"') ;
+    const result5 = service.handleSpecialCharsHtml("'") ;    
+
+    expect(result1).toBe("&amp;");
+    expect(result2).toBe("&lt;"); 
+    expect(result3).toBe("&gt;");
+    expect(result4).toBe("&quot;");
+    expect(result5).toBe("&#039;");  
+
+  })
+  
+  it("should return default poster", () => {
+    const poster = service.poster("N/A");
+    expect(poster).toBe(
+      "https://www.prokerala.com/movies/assets/img/no-poster-available.jpg"
+    );
+  });
+
 });
 
-describe('MoviesService', ()=> {
-it('should escape unsafe characters', () => {
-pending()
-})
 
-it('should return poster', () => {
-  pending()
-})
-})
